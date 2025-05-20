57292fec5b551d30b9bf911b8940dfe3e55734e0
Sathvik_PC <140819564+SathvikKS-Perspectiv@users.noreply.github.com>
2025-05-20 12:20:25 +0530

    Add additional user_info entries in seed.sql for user3 and user4

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,2 +1,3 @@
 insert into user_login (email, password) values ('test@test.com', 'test');
 insert into user_info (name) values ('user1'), ('user2');
+insert into user_info (name) values ('user3'), ('user4');
